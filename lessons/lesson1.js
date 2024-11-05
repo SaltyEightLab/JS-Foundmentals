0585c99aa35ee3c970bf6f8cccf66611a8be5c97
SaltyEightLab <hachiman_hachi@icloud.com>
2024-11-06 06:18:28 +0900

    var, let, const

---
+++ lessons/lesson1.js
@@ -0,0 +1,18 @@
+// 1.Hello World
+console.log("Hello World");
+
+// Variables
+var firstName = "John";
+let lastName = "Smith";
+console.log(firstName, lastName);
+
+var age, dateOfBirth, sex;
+age = "5";
+sex = "Male";
+console.log(age, sex);
+age = "6";
+console.log(age);
+
+//constants
+const occupation = "Engineer";
+console.log(occupation);
